83bfa9b88077e81920e6058a8ae6354d9ff39e7f
Yancy Ribbens <yancy.ribbens@gmail.com>
2014-08-12 14:06:01 -0400

    rename from acclaim to Acclaim

---
+++ acclaim/lang/en/acclaim.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for Acclaim
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_Acclaim
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Acclaim';
+$string['modulenameplural'] = 'Acclaims';
+$string['modulename_help'] = 'Use the Acclaim module for... | The Acclaim module allows...';
+$string['Acclaimfieldset'] = 'Custom example fieldset';
+$string['Acclaimname'] = 'Acclaim name';
+$string['Acclaimname_help'] = 'This is the content of the help tooltip associated with the Acclaimname field. Markdown syntax is supported.';
+$string['Acclaim'] = 'Acclaim';
+$string['pluginadministration'] = 'Acclaim administration';
+$string['pluginname'] = 'Acclaim';
