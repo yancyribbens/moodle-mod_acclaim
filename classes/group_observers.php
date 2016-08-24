511c280015f2985698bff8e37d37f758ba8fdae8
root <yancy.ribbens@gmail.com>
2016-08-24 00:10:59 -0400

    use event to create issued to first and last

--- classes/group_observers.php
+++ classes/group_observers.php
@@ -21,20 +21,22 @@
  * @copyright  2014 Yancy Ribbens
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 namespace mod_acclaim;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/mod/acclaim/locallib.php');
 class group_observers {
 
     public static function issue_badge($event) {
+	global $DB;
+        $user_id = $event->userid;
+        $user = $DB->get_record('user', array('id'=>$user_id));
+        $firstname = $user->firstname;
+        $lastname = $user->lastname;    
 
-        //var_dump($event);
-        error_log('Event Data: ' . print_r($event,true));
-        //$f = "/tmp/moodle_called_issue_badge";
-        //file_put_contents($f,"test");
-
+        //error_log('User Record: '.print_r($user,true));
+        //error_log('Event Data: ' . print_r($event,true));
 
         $org_id='6bb2e1c7-c66b-4d47-9301-4a6b9e792e2c';
         $url='https://jefferson-staging.herokuapp.com/api/v1/organizations/'.$org_id.'/badges';
@@ -44,12 +46,14 @@ class group_observers {
 
         $data = array(
             'badge_template_id' => '287d9248-89df-4ab8-af1c-71b195c494a8',
-            'issued_to_first_name' => 'yancy',
-            'issued_to_last_name' => 'ribbens',
+            'issued_to_first_name' => $firstname,
+            'issued_to_last_name' => $lastname,
             'expires_at' => '2018-04-01 09:41:00 -0500',
             'recipient_email' => 'yancy.ribbens+test_again@gmail.com',
             'issued_at' => '2014-04-01 09:41:00 -0500'
         );
+
+        error_log('Data: '.print_r($data,true));
 
         $ch = curl_init();
 
@@ -68,7 +72,6 @@ class group_observers {
         $result = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-
     }
 }
 
